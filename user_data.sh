b8ec31b169e41de4b5ab732ea07b82686d93d21f
Antriksh Devgan <antrikshdevgan@gmail.com>
2020-06-18 15:08:18 +0000

    first commit

--- user_data.sh
+++ user_data.sh
@@ -12,3 +12,14 @@ sudo apt-get update
 sudo apt-get install kubeadm kubelet kubectl -y
 sudo apt-mark hold kubeadm kubelet kubectl
 sudo swapoff -a
+
+sudo apt-get install nginx -y
+sudo wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo echo "deb https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list
+sudo apt-get update
+sudo apt install -y jenkins
+sudo systemctl enable jenkins
+
+sudo apt-get install maven -y
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre
+sudo apt-get install openjdk-8-jdk -y
